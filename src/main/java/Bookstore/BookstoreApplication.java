122cfbb446f085278115b70a5f79da5255e73ff2
Amponsah Michael <a1600626@myy.haaga-helia.fi>
2017-03-12 16:51:04 +0200

    Adding security features

--- src/main/java/Bookstore/BookstoreApplication.java
+++ src/main/java/Bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import Bookstore.domain.Book;
 import Bookstore.domain.BookRepository;
 import Bookstore.domain.CategoryRepository;
 import Bookstore.domain.Category;
+import Bookstore.domain.User;
+import Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,7 +23,7 @@ public class BookstoreApplication {
 	}
 		
 		@Bean
-		public CommandLineRunner booksDemo(BookRepository repository, CategoryRepository crepository) {
+		public CommandLineRunner booksDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 			return (args) -> {
 				log.info("save a couple of students");
 				crepository.save(new Category("Fiction"));
@@ -31,8 +33,15 @@ public class BookstoreApplication {
 				repository.save(new Book("The call that changed", "Mike REAL", 2017, "A19008", 20,crepository.findByName("Life").get(0)));
 				repository.save(new Book("Grief child", "Kofi Game", 2000, "A194874", 10, crepository.findByName("Fiction").get(0)));
 				repository.save(new Book("In the chest of a woman", "MDK", 2010, "B271225", 20, crepository.findByName("Fiction").get(0)));
-				log.info("fetch all books");
-				for (Book book : repository.findAll()) {
+				
+				// Create users: admin/admin user/user
+				User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+				User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+				urepository.save(user1);
+				urepository.save(user2);
+				
+				log.info("fetch books");
+				for (Book book: repository.findAll()){
 					log.info(book.toString());
 				}
 
--- src/main/java/Bookstore/web/BookController.java
+++ src/main/java/Bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package Bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import Bookstore.domain.Book;
 import Bookstore.domain.BookRepository;
@@ -19,12 +23,31 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 	
-	@RequestMapping(value="/booklist", method=RequestMethod.GET)
-	public String displayBook(Model model){
-		model.addAttribute("books", repository.findAll());
-		return "booklist";
-	}
+	// Show all students
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
+    
+	@RequestMapping(value="/booklist")
+    public String displayBookStore(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
 	
+	// RESTful service to get all books   ,  it will show all students in JSON form
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();	
+    }    
+	
+ // RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);	
+    } 
+ // Add new student
+   // @PreAuthorize("hasAuthority('ADMIN')")
 	@RequestMapping (value = "/add")
 	public String addBook(Model model){
 		model.addAttribute("book", new Book());
